97fe67ca228183b9f846c61e1424cbd153473510
smccaughna <150574058+smccaughna@users.noreply.github.com>
2024-02-19 11:57:37 -0800

    Delete out/build/x64-release directory

--- out/build/x64-release/_deps/sdl2-build/include/SDL2/SDL_revision.h
+++
@@ -1,8 +0,0 @@
-/* #undef SDL_VENDOR_INFO */
-#define SDL_REVISION_NUMBER 0
-
-#ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-prerelease-2.29.2-46-g646cccede (" SDL_VENDOR_INFO ")"
-#else
-#define SDL_REVISION "SDL-prerelease-2.29.2-46-g646cccede"
-#endif
